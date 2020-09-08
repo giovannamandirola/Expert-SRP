3b88e2dca3b400179df447ed9cf6aa080cb7d79f
giovannamandirola <69222964+giovannamandirola@users.noreply.github.com>
2020-09-08 19:47:13 -0300

    Ejercicio 4 Game of Life

---
+++ Ejercicio 4/src/Library/ImprimirTablero.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+
+namespace Library
+{
+    public class ImprimirTablero
+    {
+        public static void ImprimirConsola (GameBoard g) 
+        {
+            int width = g.gameBoard.GetLength(0);
+            int height = g.gameBoard.GetLength(1);
+            
+            while (true)
+            {
+                Console.Clear();
+                StringBuilder s = new StringBuilder();
+                for (int y = 0; y<height;y++)
+                {
+                    for (int x = 0; x<width; x++)
+                    {
+                        if(g.gameBoard[x,y])
+                        {
+                            s.Append("|X|");
+                        }
+                        else
+                        {
+                            s.Append("___");
+                        }
+                    }
+                    s.Append("\n");
+                }
+                Console.WriteLine(s.ToString());
+
+                g.NewGameBoard();
+                
+            Thread.Sleep(300);
+            }
+        }
+       
+    }
+}
---
+++ Ejercicio 4/src/Program/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Library;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool[,] a;
+            a = Archivo.LeerArchivo("C:\\Users\\Administrador\\Documents\\Programación\\Expert-SRP\\Ejercicio 4\\board.txt");
+            GameBoard g = new GameBoard(a);
+            ImprimirTablero.ImprimirConsola(g);
+        }
+    }
+}
